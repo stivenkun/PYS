88fb2de1b931edbfc5b9d675e2b2f5320f4071bd
Tulio Jimenez <tulio.jimenez@exsis.com.co>
2018-09-26 17:04:40 -0500

    commit repositorios nuevo (net core)

---
+++ AccessData/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessData
+{
+    public class Class1
+    {
+    }
+}
