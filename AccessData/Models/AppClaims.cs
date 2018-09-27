0e8e39673af557ed9c6d47643dddd37ebe5914ca
Tulio Jimenez <stivenkun@gmail.com>
2018-09-26 23:07:42 -0500

    added file

---
+++ AccessData/Models/AppClaims.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace AccessData.Models
+{
+    public class AppClaims
+    {
+        [Key]
+        public int Id { set; get; }
+
+        [Required]
+        public string Name { set; get; }
+
+        public string Description { set; get; }
+
+        [Required]
+        public string Value { set; get; }
+    }
+}
