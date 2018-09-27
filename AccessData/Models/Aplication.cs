c273f1c1c09897a6384192c324f4283cbfe5ff41
Tulio Jimenez <stivenkun@gmail.com>
2018-09-26 22:54:41 -0500

    model file

---
+++ AccessData/Models/Aplication.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace AccessData.Models
+{
+    public class Aplication
+    {
+        [Key]
+        public int Id { set; get; }
+
+        [Required]
+        public string Name { set; get; }
+
+        [Url]
+        [Required]
+        public string Url { set; get; }
+ 
+        public string Description { set; get; }
+
+        public bool IsActive { set; get; }
+
+        public string IconSrc { set; get; }
+    }
+}
