0ad66133e31ef5cf8a2f9988702a66b22ab19283
Tulio Jimenez <stivenkun@gmail.com>
2018-09-27 22:48:52 -0500

    aplication repository

---
+++ Repository/Repositories/ApplicationRepository.cs
@@ -0,0 +1,17 @@
+﻿using AccessData.Datas;
+using AccessData.Models;
+using Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.Repositories
+{
+    public class ApplicationRepository : GenericRepository<Aplication>, IApplicationRepository
+    {
+        public ApplicationRepository(ApplicationDbContext context) : base(context)
+        {
+
+        }
+    }
+}
---
+++ src/PYS.IdentityServer.Security.Administration/Data/Migrations/IdentityServer/ConfigurationDb/20180928022657_newTables.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PYS.IdentityServer.Security.Administration.Data.Migrations.IdentityServer.ConfigurationDb
+{
+    public partial class newTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+          
+
+            migrationBuilder.CreateTable(
+                name: "Applications",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: false),
+                    Url = table.Column<string>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    IsActive = table.Column<bool>(nullable: false),
+                    IconSrc = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Applications", x => x.Id);
+                });
+
+            
+            migrationBuilder.CreateTable(
+                name: "AppClaims",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    Value = table.Column<string>(nullable: false),
+                    ApplicationId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppClaims", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AppClaims_Applications_ApplicationId",
+                        column: x => x.ApplicationId,
+                        principalTable: "Applications",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppClaims_ApplicationId",
+                table: "AppClaims",
+                column: "ApplicationId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AppClaims");
+
+            migrationBuilder.DropTable(
+                name: "Applications");
+        }
+    }
+}
