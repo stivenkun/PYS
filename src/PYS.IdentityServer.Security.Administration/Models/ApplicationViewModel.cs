c10e02aff8245a4c4e0176456083d33f4df4779a
Tulio Jimenez <tulio.jimenez@exsis.com.co>
2018-10-03 15:00:34 -0500

    save claim session

---
+++ src/PYS.IdentityServer.Security.Administration/Models/ApplicationViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PYS.IdentityServer.Security.Administration.Models
+{
+    public class ApplicationViewModel
+    {
+        public AccessData.Models.Aplication Application { set; get; }
+        public AccessData.Models.AppClaims AppClaims { set;get }
+    }
+}
